282f5e56db4000464e4a68f1b2b9f44a6a74f248
Abiagam Onyesi <onyesi-john.abiagam@stud.hshl.de>
2025-02-15 14:10:59 +0100

    corrected errors

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load YOLO model
-model = YOLO("yolov11.pt")  # Update if you use a different weight file
+model = YOLO("yolov8n.pt")  # Update if you use a different weight file
 
 # Train model
 model.train(data="dataset/data.yaml", epochs=50, imgsz=640)
