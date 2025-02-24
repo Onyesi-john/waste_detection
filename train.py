62172041007ecbae5f06800ea8ccfb0acc846d0f
Abiagam Onyesi <onyesi-john.abiagam@stud.hshl.de>
2025-02-24 17:32:08 +0100

    updates

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ model = YOLO("yolov5nu.pt")
 
 # Train the model
 model.train(
-    data="/home/john/waste_detection/dataset/data.yaml",  # Relative path to data.yaml
+    data="/home/circleci/project/dataset/data.yaml",  # Relative path to data.yaml
     epochs=50,  # Changed from 15 to 50
     imgsz=416,
     batch=8,
