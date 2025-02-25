c7133d80a7ab88e3094bddafe892b1fb742d3023
Abiagam Onyesi <onyesi-john.abiagam@stud.hshl.de>
2025-02-25 15:53:56 +0100

    errors improved

--- train.py
+++ train.py
@@ -1,17 +1,19 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov5nu.pt")
+model = YOLO("/home/john/waste_detection/yolov5nu")
 
 # Train the model
 model.train(
-    data="data.yaml",  # Relative path to data.yaml
+    data="/home/john/waste_detection/dataset/data.yaml",  # Relative path to data.yaml
     epochs=50,  # Changed from 15 to 50
     imgsz=416,
     batch=8,
-    workers=2
+    workers=2,
+    project="./yolo5nu",  # Save results in the "yolo5nu" folder
+    name="",  # Empty name to avoid subfolders
+    exist_ok=True  # Overwrite existing files
 )
 
-# The best model is automatically saved to runs/detect/train/weights/best.pt
-print("Training completed. Best model saved to runs/detect/train/weights/best.pt")
-
+# The best model is automatically saved to yolo5nu/weights/best.pt
+print("Training completed. Best model saved to yolo5nu/weights/best.pt")
