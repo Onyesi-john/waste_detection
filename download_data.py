a27d41881da743ab1b3e7973785bef84791445fa
Abiagam Onyesi <onyesi-john.abiagam@stud.hshl.de>
2025-02-26 10:19:22 +0100

    updating dataset

--- download_data.py
+++ download_data.py
@@ -2,25 +2,16 @@ import gdown
 import zipfile
 import os
 
-FILE_ID = "1j4aV5SU5jRFsHHz3bSgJSN-16o1rIjy8"
+# Google Drive File ID (Replace with yours)
+FILE_ID = "1-3KPCCqF2kjNElo9f9L6QwricRqe0PKq"
 OUTPUT_ZIP = "dataset.zip"
-OUTPUT_FOLDER = "dataset"
 
 # Download dataset
-print("📥 Downloading dataset...")
 gdown.download(f"https://drive.google.com/uc?id={FILE_ID}", OUTPUT_ZIP, quiet=False)
 
-# Check if file exists
-if not os.path.exists(OUTPUT_ZIP):
-    raise FileNotFoundError(f"❌ Download failed! {OUTPUT_ZIP} not found.")
-
 # Extract dataset
-print("📂 Extracting dataset...")
-try:
-    with zipfile.ZipFile(OUTPUT_ZIP, "r") as zip_ref:
-        zip_ref.extractall(OUTPUT_FOLDER)
-except zipfile.BadZipFile:
-    raise ValueError("❌ The downloaded ZIP file is corrupted!")
+with zipfile.ZipFile(OUTPUT_ZIP, "r") as zip_ref:
+    zip_ref.extractall("dataset")
 
 # Remove zip file
 os.remove(OUTPUT_ZIP)
