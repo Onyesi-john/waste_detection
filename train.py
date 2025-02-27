665c9382cac9cdd7cec0da27902f977029d38dfc
Abiagam Onyesi <onyesi-john.abiagam@stud.hshl.de>
2025-02-27 06:12:08 +0100

    correction to config

--- train.py
+++ train.py
@@ -5,11 +5,9 @@ model = YOLO("yolov5nu.pt")
 
 # Train the model
 model.train(
-    data="/home/circleci/project/dataset/data.yaml",  # Update this path
+    data="/home/john/waste_detection/dataset/data.yaml",  # Update this path
     epochs=50,
     batch=8,
     imgsz=416,
-    project="/home/circleci/project/waste_detection",
-    name="exp",
-    exist_ok=True,
+    
 )
